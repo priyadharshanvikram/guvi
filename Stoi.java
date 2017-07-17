40c7262310940fde1e76fe64934916b2f31db945
priyadharshanvikram <priyadharshanvikram@gmail.com>
2017-07-17 11:20:58 +0530

    Add files via upload

---
+++ Stoi.java
@@ -0,0 +1,13 @@
+package nobiee007;
+
+import java.util.Scanner;
+
+public class Stoi {
+
+	public static void main(String[] args) {
+     Scanner s=new Scanner(System.in);
+     int i=Integer.parseInt(s.next());
+     System.out.println("Converted Integer is : "+i);
+	}
+
+}
