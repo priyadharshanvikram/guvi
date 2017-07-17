8d7752350557b95e616872241f529acb8945ec61
priyadharshanvikram <priyadharshanvikram@gmail.com>
2017-07-17 09:44:19 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,12 @@
+package nobiee007;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		String str="makeover";
+		String reverse=new StringBuffer(str).reverse().toString();
+		System.out.println("Reversed String is : "+reverse);
+
+	}
+
+}
