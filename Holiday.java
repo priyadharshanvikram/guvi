29721d3c3f018ec830092024cb5e638af03e693a
priyadharshanvikram <priyadharshanvikram@gmail.com>
2017-07-17 14:39:47 +0530

    Add files via upload

---
+++ Holiday.java
@@ -0,0 +1,21 @@
+package nobiee007;
+
+import java.util.Scanner;
+
+public class Holiday {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String s1=s.next();
+		if(s1.equalsIgnoreCase("saturday")||s1.equalsIgnoreCase("sunday")){
+	    	System.out.println("false");
+	    }else{
+	    	System.out.println("true");
+	    }
+		
+		
+			
+		
+	}
+
+}
