b6ef9d58d55f20bf6759aa3bf4f3c4f0747c999f
priyadharshanvikram <priyadharshanvikram@gmail.com>
2017-07-24 15:39:33 +0530

    Add files via upload

---
+++ Natural.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class Natural {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int a=0;
+		for(int i=1;i<=n;i++){
+			a+=i;
+		}
+		System.out.println("sum of natural numbers is : "+a);
+
+	}
+
+}
