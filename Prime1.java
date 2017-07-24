e633b767f6070006b9f06fe6ec218580cf6b3bae
priyadharshanvikram <priyadharshanvikram@gmail.com>
2017-07-24 16:08:40 +0530

    Add files via upload

---
+++ Prime1.java
@@ -0,0 +1,22 @@
+package nobiee007;
+
+import java.util.Scanner;
+
+public class Prime1 {
+
+	public static void main(String[] args) {
+		 Scanner s=new Scanner(System.in);
+	     int n=s.nextInt();
+	     int count = 0,a1;
+	     int i=1;
+	    while(i<n){
+	    	if(n%i!=0)
+	         count++;
+	    		i++;
+	    }
+	    System.out.println(count);
+		}
+
+	
+
+}
