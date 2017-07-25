dc83ba0f479f3cbbcc79435dec1e2e5575984a3e
priyadharshanvikram <priyadharshanvikram@gmail.com>
2017-07-25 14:46:38 +0530

    Add files via upload

---
+++ Oddbit.java
@@ -0,0 +1,21 @@
+package nobiee007;
+
+import java.util.Scanner;
+
+public class Oddbit {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter thr range : ");
+		int a=s.nextInt();
+		int b=s.nextInt();
+		for(int i=a;i<=b;i++){
+			if(i%2!=0){
+				System.out.print(i+" ");
+			}
+		}
+		
+
+	}
+
+}
